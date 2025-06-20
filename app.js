78c21375ef8f2793726cfc1802c7b69dd881cf80
Junaid Hassan <junaidhassan225588@gmail.com>
2025-06-20 10:01:35 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const flash = require('connect-flash');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
+app.set('layout', 'layout');
+
+// View engine setup
+app.set('view engine', 'ejs');
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+});
+
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
